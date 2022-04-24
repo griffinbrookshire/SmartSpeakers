616daa117a65c1fa4d71263360cefada2e6d19fc
matthew pudlo <mrpudlo@ncsu.edu>
2022-04-24 09:41:27 -0400

    added auto refresh

--- lcd_service.py
+++ lcd_service.py
@@ -1,14 +1,21 @@
-from flask import Flask, request, render_template
-
+from flask import Flask, render_template, request, redirect, url_for
+from turbo_flask import Turbo
+import datetime
 app = Flask(__name__)
+turbo = Turbo(app)
+
+title = None
+artist = None
 
 @app.route('/update_song', methods=["POST"])
 def update_song():
     title = request.get_json().get('title')
     artist = request.get_json().get('artist')
     print(title + " " + artist)
-    return render_template('index.html')
+    turbo.push(turbo.update(render_template('index.html', title, artist)))
+    return 'success'
 
 
 if __name__ == '__main__':
     app.run(debug=False, host="0.0.0.0")
+
