3b321139dcafe185822a57ff4af4964844a8309a
matthew pudlo <mrpudlo@ncsu.edu>
2022-04-24 09:31:32 -0400

    add song tile and artist to screen

--- lcd_service.py
+++ lcd_service.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -7,7 +7,7 @@ def update_song():
     title = request.get_json().get('title')
     artist = request.get_json().get('artist')
     print(title + " " + artist)
-    return 'success'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
