acbdcd1d63587a3face78cc6c6eb58b25d82c056
John Bumgardner <john.bumgardner@icloud.com>
2022-04-24 14:36:37 -0400

    bigfix

--- lcd_service.py
+++ lcd_service.py
@@ -9,15 +9,17 @@ artist = None
 
 @app.route('/update_song', methods=["POST"])
 def update_song():
+    global title, artist
     title = request.get_json().get('title')
     artist = request.get_json().get('artist')
     print(title + " " + artist)
-    turbo.push(turbo.update(render_template('index.html', title = title,  artist = artist)))
+    turbo.push(turbo.update(render_template('data.html', title = title, artist = artist), 'data'))
     return 'success'
 
 @app.route('/')
 def root():
-    return render_template('index.html', title = 'Not Playing', artist = "----")
+    
+    return render_template('index.html', title = title, artist = artist)
 
 if __name__ == '__main__':
     app.run(debug=False, host="0.0.0.0")
